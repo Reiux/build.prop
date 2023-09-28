4deee6ff6aac67999c352ac545e86682cd86c9f1
Kazumi <90865965+Reiux@users.noreply.github.com>
2023-09-29 00:31:30 +0900

    Update download_latest_ota_build.sh

--- download_latest_ota_build.sh
+++ download_latest_ota_build.sh
@@ -12,7 +12,7 @@ print_message "Downloading OTA builds for the following devices: $(IFS=, ; echo 
 
 # Get download link from the latest build that is not mobile carrier restricted {33} chars to be expected.
 for device_name in "$@"; do # Allow multiple arguments
-  last_build_url=$(curl -Ls 'https://developers.google.com/android/ota?partial=1' | grep -Eo "\"(\S+${device_name}\S{33})?zip" | tail -1 | tr -d \")
+  last_build_url=$(curl -Ls 'https://developers.google.cn/android/ota?partial=1' | grep -Eo "\"(\S+${device_name}\S{33})?zip" | tail -1 | tr -d \")
   print_message "Downloading OTA build for ${device_name^} (\"$last_build_url\")..." debug
   wget --tries=inf --show-progress -q "$last_build_url"
 done
